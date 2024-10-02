ef21a698f53ec592ef8116456baf71d39a6c4a1c
Raphael <rafagomes1706@gmail.com>
2024-10-02 20:58:13 -0300

    first commit

---
+++ src/main/java/com/raphael/ReadingManagement/ReadingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.raphael.ReadingManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raphael/ReadingManagement/ReadingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphael.ReadingManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
