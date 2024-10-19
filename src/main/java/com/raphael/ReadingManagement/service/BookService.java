e8d3a628202c66a8c141c92c48e5e8b448ea951f
Raphael Gomes <rafagomes1706@gmail.com>
2024-10-19 17:57:35 -0300

    refactor

--- src/main/java/com/raphael/ReadingManagement/service/BookService.java
+++ src/main/java/com/raphael/ReadingManagement/service/BookService.java
@@ -7,7 +7,6 @@ import com.raphael.ReadingManagement.model.Book;
 import com.raphael.ReadingManagement.model.Library;
 import com.raphael.ReadingManagement.model.Reader;
 import com.raphael.ReadingManagement.repository.BookRepository;
-import com.raphael.ReadingManagement.repository.LibraryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -20,27 +19,16 @@ public class BookService {
 
     private final LibraryService libraryService;
 
-    private final LibraryRepository libraryRepository;
-
     public void createBook(BookRequestDTO requestDTO, Reader reader) {
 
         Library library = libraryService.getLibrary(requestDTO.getLibraryName(), reader.getReaderId());
 
-        Book book = BookBuilder.from(requestDTO, library);
-
-        addBook(library, book);
-
-        bookRepository.save(book);
-    }
-
-    private void addBook(Library library, Book newBook) {
+        Book newBook = BookBuilder.from(requestDTO, library);
 
         if (library.getBooks().stream().anyMatch(book -> book.getName().equals(newBook.getName()))) {
             throw new LibraryException("O livro já foi cadastrado nessa biblioteca", HttpStatus.BAD_REQUEST);
         }
 
-        library.getBooks().add(newBook);
-
-        libraryRepository.save(library);
+        bookRepository.save(newBook);
     }
 }
