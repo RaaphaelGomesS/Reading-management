31d7642d5fd1457f9145007aea8b402465e2b1fe
RaaphaelGomesS <rafagomes1706@gmail.com>
2024-11-21 15:24:31 -0300

    first commit

---
+++ src/main/java/com/raphael/Reading/Management/application/ReadingManagementApplication.java
@@ -0,0 +1,14 @@
+package com.raphael.Reading.Management.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingManagementApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ReadingManagementApplication.class, args);
+    }
+
+}
