ae94185d03a8fc1944d7f5fcb5f4eaee61a508ad
RaaphaelGomesS <rafagomes1706@gmail.com>
2024-12-06 17:32:46 -0300

    add security filter and token generation/validation

---
+++ src/main/java/com/raphael/Reading/Management/config/SecurityFilter.java
@@ -0,0 +1,51 @@
+package com.raphael.Reading.Management.config;
+
+import com.raphael.Reading.Management.repository.ReaderRepository;
+import com.raphael.Reading.Management.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private ReaderRepository repository;
+
+    private TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recoveryToken(request);
+
+        if(tokenJWT != null) {
+            String subject = tokenService.getSubject(tokenJWT);
+            UserDetails user = repository.findByUsername(subject);
+
+            var auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoveryToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
+        }
+
+        return null;
+    }
+}
--- src/main/java/com/raphael/Reading/Management/controller/LoginController.java
+++ src/main/java/com/raphael/Reading/Management/controller/LoginController.java
@@ -3,10 +3,14 @@ package com.raphael.Reading.Management.controller;
 import com.raphael.Reading.Management.dto.LoginRequest;
 import com.raphael.Reading.Management.dto.ReaderDTO;
 import com.raphael.Reading.Management.dto.ReaderDTORequest;
+import com.raphael.Reading.Management.entity.Reader;
 import com.raphael.Reading.Management.service.AuthenticationService;
+import com.raphael.Reading.Management.service.TokenService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +23,10 @@ public class LoginController {
 
     private AuthenticationService service;
 
+    private TokenService tokenService;
+
+    private AuthenticationManager manager;
+
     @PostMapping("/create")
     public ResponseEntity<ReaderDTO> createAccount(@RequestBody @Valid ReaderDTORequest request) {
 
@@ -27,8 +35,15 @@ public class LoginController {
         return ResponseEntity.ok(readerDTO);
     }
 
-//    @PostMapping("/login")
-//    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
-//
-//    }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+
+        var authDTO = new UsernamePasswordAuthenticationToken(request.username(), request.password());
+
+        var auth = manager.authenticate(authDTO);
+
+        String token = tokenService.generateToken((Reader) auth.getPrincipal());
+
+        return ResponseEntity.ok(token);
+    }
 }
